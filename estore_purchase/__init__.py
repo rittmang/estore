041bdfdc3cb411709d1d7bd06b09bc8b056020a2
rittmang <ritomgupta99@gmail.com>
2019-12-03 09:59:56 +0530

    Finished config of init

--- estore_purchase/__init__.py
+++ estore_purchase/__init__.py
@@ -1,6 +1,23 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, redirect, request, url_for, abort
+import os
+from flask_bcrypt import Bcrypt
+from flask_login import (
+    LoginManager,
+    login_required,
+    login_user,
+    logout_user,
+    current_user
+)
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import desc,exc
 app=Flask(__name__)
+app.secret_key=os.getenv("SECRET_KEY")
+db=SQLAlchemy(app)
+login_manager  = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view="login"
+bcrypt=Bcrypt(app)
+app.config["SQLALCHEMY_DATABASE_URI"]=os.getenv("DATABASE_URL")
 @app.route("/")
 def root():
     return render_template(
