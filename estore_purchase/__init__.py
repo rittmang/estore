1966bedb64bf949eccb014c5df65642917d4292d
rittmang <ritomgupta99@gmail.com>
2019-12-05 09:39:43 +0530

    add post stuff for register

--- estore_purchase/__init__.py
+++ estore_purchase/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request, url_for, abort
+from flask import Flask, render_template, redirect, request, url_for, jsonify,abort
 import os
 from flask_bcrypt import Bcrypt
 from flask_login import (
@@ -18,7 +18,10 @@ login_manager.init_app(app)
 login_manager.login_view="login"
 bcrypt=Bcrypt(app)
 app.config["SQLALCHEMY_DATABASE_URI"]=os.getenv("DATABASE_URL")
-
+#  INSERT INTO users (email, password) VALUES (
+#   'johndoe@mail.com',
+#   crypt('johnspassword', gen_salt('bf'))
+# );
 @app.route("/")
 def root():
     return render_template(
@@ -30,8 +33,24 @@ def login():
     return render_template(
         "login.html"
     )
-@app.route("/register")
+@app.route("/register",methods=["GET","POST"])
 def register():
+    
+    if request.method=='POST':
+        try:
+            name=request.form["name"]
+            username=request.form["username"]
+            password=request.form["password"]
+        except KeyError:
+            return jsonify({"response":"Enter all data please"}),400
+    u=Users(
+        name=name,
+        username=username,
+        password=password,
+    )
+    try:
+        db.session.add(u)
+        db.session.commit()
     return render_template(
         "register.html"
     )
